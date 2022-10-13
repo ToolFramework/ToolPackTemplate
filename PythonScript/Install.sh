586a605ad5a48190dc17868e32bdf174ec2aad38
Marcus O'Flaherty <moflaherty1@sheffield.ac.uk>
2022-10-13 19:22:43 +0100

    install script generalisations

--- PythonScript/Install.sh
+++ PythonScript/Install.sh
@@ -1,114 +1,352 @@
 #!/bin/bash
 #set -x
 #set -e
-# sanity checks
-# check we're root (required for installing dependencies)
-if [ "$(whoami)" != "root" ]; then
-	echo "This script must be run as root"
-	exit 1
-fi
-
-# check we have a non-root user for installing pip dependencies
-TOOLUSER="tool"
-USERINVALID=`id -u ${TOOLUSER} &> /dev/null; echo $?`
-while [ ${USERINVALID} -ne 0 ]; do
-	echo "user 'tool' not found"
-	echo "do you wish to install cppyy in another user's directory?"
-	select result in Yes No; do
-		if [ "$result" == "Yes" ] || [ "$result" == "No" ]; then
-			if [ "$result" == "No" ]; then
-				echo "terminating.";
-				exit 1;
-			else
-				echo "enter the user whose home directory you would like to install cppyy into"
-				read TOOLUSER
-				USERINVALID=`id -u ${TOOLUSER} &> /dev/null; echo $?`
-			fi
-		else
-			echo "please enter 1 or 2";
-		fi
-	done
-done
-echo "installing cppyy for user ${TOOLUSER}"
 
 CURDIR=${PWD}
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 # check the ToolFramework folder exists and is writable
 #TOOLFRAMEWORKDIR="/opt/ToolFrameworkCore"
-TOOLFRAMEWORKDIR=$(readlink -f ${PWD}/../../../..)
-if [ ! -d ${TOOLFRAMEWORKDIR} ] || [ ! -w ${TOOLFRAMEWORKDIR} ]; then
+# assuming this script is 
+# ../ToolFrameworkApplication/UserTools/ImportedTools/ToolPack/PythonScript/Install.sh
+# then the base ToolFrameworkApplication directory should be:
+TOOLFRAMEWORKDIR=$(readlink -f ${CURDIR}/../../../..)
+SANITYCHECK=$(ls ${TOOLFRAMEWORKDIR}/UserTools >/dev/null 2>&1; echo $?)
+if [ ! -d ${TOOLFRAMEWORKDIR} ] || [ ! -w ${TOOLFRAMEWORKDIR} ] || [ ${SANITYCHECK} -ne 0 ]; then
 	# doesn't exist or isn't writable (perhaps an immutable container?)
 	echo "ToolFramework directory ${TOOLFRAMEWORKDIR} is not present or is not writable!"
 	exit 1
 fi
-DEPENDENCIESDIR=${TOOLFRAMEWORKDIR}/Dependencies
-# make dependencies dir it if it doesn't exist... is this sensible?
-mkdir -p ${DEPENDENCIESDIR}
 
-# make non-root user home directory (installation dir of pip installs), if it doesn't exist
-if [ ! -d /home/${TOOLUSER} ]; then
-	echo "making home directory for user ${TOOLUSER}"
-	mkdir -p /home/${TOOLUSER}
-	chown -R `id -u ${TOOLUSER}` /home/${TOOLUSER}
+# first determine what kind of OS we're on.
+REDHATLIKE=$(yum --version >/dev/null 2>&1; echo $?)
+DEBIANLIKE=$(dpkg --version >/dev/null 2>&1; echo $?)
+NEEDDEPS=""
+if [ ${REDHATLIKE} -eq 0 ]; then
+	echo "red-hat based OS"
+	# red-hat based OS.
+	DEPS=(gcc-c++ gcc make cmake git python3 python3-libs python3-devel python3-pip patch which)
+	for DEP in "${DEPS[@]}"; do
+		echo "looking for ${DEP}..."
+		# this is probably not a great check as 'grep' could match on anything
+		# e.g. some python3 add-on, even if python3 is installed, but it'll do.
+		# XXX moreover, it will NOT find things like 'rh-python38-python-libs.x86_64'
+		# as would be the case if python-3.8 is installed via epel repos, or somesuch
+		GOTDEP=$(yum list installed | grep "${DEP}" >/dev/null 2>&1; echo $?)
+		if [ ${GOTDEP} -ne 0 ]; then
+			echo "not found"
+			NEEDDEPS="${NEEDDEPS} ${DEP}"
+		fi
+	done
+elif [ ${DEBIANLIKE} -eq 0 ]; then
+	echo "debian based OS"
+	DEPS=(gcc g++ make cmake git libpython3 python3-dev python3-pip patch debian-utils)
+	# per 'dpkg -S $(readlink `which which`)', which is provided by debian-utils, at least in debian.
+	for DEP in "${DEPS[@]}"; do
+		echo "looking for ${DEP}..."
+		GOTDEP=$(dpkg --list | grep "${DEP}" >/dev/null 2>&1; echo $?)
+		if [ ${GOTDEP} -ne 0 ]; then
+			echo "not found"
+			NEEDDEPS="${NEEDDEPS} ${DEP}"
+		fi
+	done
+else
+	# can't recognise the system to check what's installed, or install if necessary...
+	# ask the user if they want to continue under the assumption we have everything
+	echo "Python tools require the following system dependencies:"
+	echo "gcc-c++ gcc make cmake git python3 python3-libs python3-devel python3-pip which"
+	echo "If all of these packages are installed, enter 1 to continue to attempt installation"
+	echo "Otherwise enter 2 to abort and re-run this script once those dependencies are installed"
+	select result in Continue Abort; do
+		if [ "$result" == "Continue" ] || [ "$result" == "Abort" ]; then
+			if [ "$result" == "Abort" ]; then
+				echo "terminating.";
+				exit 1;
+			else
+				echo "Attempting installation assuming dependencies are installed";
+				NEEDDEPS=""
+				break;
+			fi
+		else
+			echo "please enter 1 or 2";
+		fi
+	done
 fi
 
-echo "installing dependencies"
-yum install -y gcc-c++ gcc make cmake git python36 python36-libs python36-devel python36-pip patch which sudo
+if [ -z "${NEEDDEPS}" ]; then
+	echo "All system dependencies satisfied, continuing"
+else
+	echo "The following system dependencies were not found on your system"
+	echo "${NEEDDEPS}"
+	# if we're run as root, offer to install them
+	if [ "$(whoami)" == "root" ]; then
+		echo "Install these packages?"
+		select result in Yes No; do
+			if [ "$result" == "Yes" ] || [ "$result" == "No" ]; then
+				if [ "$result" == "No" ]; then
+					echo "It is possible the dependencies are installed and were not detected by the scan. "
+					echo -n "If this is the case, and you wish to continue with installation, enter 1. "
+					echo "Otherwise to abort installation, enter 2."
+					select result in Continue Abort; do
+						if [ "$result" == "Continue" ] || [ "$result" == "Abort" ]; then
+							if [ "$result" == "Abort" ]; then
+								echo "terminating.";
+								exit 1;
+							else
+								echo "Attempting installation assuming dependencies are installed"
+								NEEDDEPS=""
+								break;
+							fi
+						else
+							echo "please enter 1 or 2";
+						fi
+					done
+					if [ -z "${NEEDDEPS}" ]; then break; fi
+				else
+					echo "Installing dependencies..."
+					if [ ${REDHATLIKE} -eq 0 ]; then
+						yum install -y "${NEEDDEPS}"
+						INSTALLOK=$?
+					elif [ ${DEBIANLIKE} -eq 0 ]; then
+						apt-get install -y "${NEEDDEPS}"
+						INSTALLOK=$?
+					fi
+					if [ ${INSTALLOK} -ne 0 ]; then
+						echo "Errors encountered; please install dependencies manually" \
+						     " and re-run this script"
+						exit 1;
+					else
+						echo "Dependencies installed successfully"
+						break;
+					fi
+				fi
+			else
+				echo "please enter 1 or 2";
+			fi
+		done
+	else
+		# we need dependencies but we're not root so can't install them
+		echo "Please re-run this script as root, or install these packages manually and re-run this script"
+		exit 1;
+	fi
+fi
+
+# XXX TODO replace all this user-directory nonsense with
+# --prefix, --root or --target
+# see https://www.mankier.com/1/pip-install
+
+# See if there's a 'tooluser' user, which should be present on ToolFramework-based containers
+USERVALID=`id -u "tooluser" &> /dev/null; echo $?`
+if [ "${USERVALID}" -eq 0 ]; then
+	TOOLUSER="tooluser"
+fi
+
+# find out who is invoking this script
+THISUSER=$(whoami)
+# if the current user is root, try to identify an
+# underlying user calling this script with `sudo`
+# (pip recommends not installing as root)
+if [ "${THISUSER}" == "root" ]; then
+	if [ ! -z "${SUDO_USER}" ]; then
+		THISUSER="${SUDO_USER}"
+	elif [ $(logname 1>/dev/null 2>&1; echo $?) -eq 0 ]; then
+		THISUSER="$(logname)"
+	fi
+	# note both of these can fail if e.g. you're in a container
+	# run with `sudo singularity shell --cleanenv`
+fi
+
+# if the user wants to install pip dependencies into another account
+# we'll need sudo
+GOTSUDO=`sudo whoami >/dev/null 2>&1; echo $?`
+SUDOCMD=""
+if [ ${GOTSUDO} -eq 1 ] && [ "$(whoami)" != "root" ]; then
+	SUDOCMD="sudo "
+fi
+
+# we don't need to be root to pip install system-wide, right...?
+OPTIONS="System ${THISUSER}"
+if [ ${GOTSUDO} -eq 0 ]; then
+	OPTIONS="${OPTIONS} ${TOOLUSER} Other"
+fi
+OPTIONS="${OPTIONS} Abort"
+
+# ask the user where they want to install pip packages
+echo "Where would you like to install pip packages?"
+select result in ${OPTIONS}; do
+	if [ "$result" == "System" ] || [ "$result" == "${THISUSER}" ] || [ "$result" == "${TOOLUSER}" ] || [ "$result" == "Other" ] || [ "$result" == "Abort" ]; then
+		if [ "$result" == "Abort" ]; then
+			echo "Aborting"
+			exit 1;
+		elif [ "$result" == "System" ]; then
+			unset PIPFLAGS
+			unset SUDOFLAGS
+			break;
+		elif [ "${result}" == "${THISUSER}" ]; then
+			PIPFLAGS="--user"
+			unset SUDOFLAGS
+			break;
+		elif [ "${result}" == "${TOOLUSER}" ]; then
+			THISUSER=${TOOLUSER}
+			PIPFLAGS="--user"
+			SUDOFLAGS="sudo -u ${THISUSER} -E"
+			break;
+		else
+			echo "enter the user whose home directory you would like to install cppyy into"
+			read THISUSER
+			USERVALID=`id -u ${THISUSER} &> /dev/null; echo $?`
+			if [ "${USERVALID}" -ne 0 ]; then
+				echo "unrecognised user"
+			else
+				PIPFLAGS=" --user"
+				SUDOFLAGS="sudo -u ${THISUSER} -E"
+				break;
+			fi
+		fi
+	else
+		echo "please enter a number corresponding to the desired user or action";
+	fi
+done
+
+if [ "${PIPFLAGS}" == "--user" ]; then
+	echo "pip packages will be installed into home directory of user ${THISUSER}"
+else
+	echo "pip packages will be installed system-wide"
+fi
+echo "Proceed?"
+select result in Proceed Abort; do
+	if [ "$result" == "Proceed" ] || [ "$result" == "Abort" ]; then
+		if [ "$result" == "Abort" ]; then
+			echo "terminating.";
+			exit 1;
+		else
+			break;
+		fi
+	else
+		echo "please enter 1 or 2";
+	fi
+done
+
+set -e
+#set -x
+
+# make home directory of desired user if it doesn't exist
+if [ "${PIPFLAGS}" == "--user" ] && [ ! -d /home/${THISUSER} ]; then
+	echo "making home directory for user ${THISUSER}"
+	$SUDOCMD mkdir -p /home/${THISUSER}
+	chown -R `id -u ${THISUSER}` /home/${THISUSER}
+fi
 
 # upgrade pip (required for --no-use-pep517 flag)
 echo "updating pip"
-sudo -u ${TOOLUSER} python3 -m pip install --user --upgrade pip
+$SUDOFLAGS python3 -m pip install $PIPFLAGS --upgrade pip
+$SUDOFLAGS python3 -m pip install $PIPFLAGS --upgrade pip
 # we'll also need wheel, or the CPyCppyy install will fail
-sudo -u ${TOOLUSER} python3 -m pip install wheel
+$SUDOFLAGS python3 -m pip install $PIPFLAGS wheel
 
 # setup installation environment
-cd ${DEPENDENCIESDIR}
 export STDCXX=11
 #export EXTRA_CLING_ARGS='-nocudainc'
 export CLING_REBUID_PCH=1
-export PATH=/home/${TOOLUSER}/.local/bin:$PATH
-#export LD_LIBRARY_PATH=/home/${TOOLUSER}/.local/lib/python3.6/site-packages/cppyy_backend/lib:$LD_LIBRARY_PATH
+export PATH=/home/${THISUSER}/.local/bin:$PATH
+DEPENDENCIESDIR=${TOOLFRAMEWORKDIR}/Dependencies
+
+# make dependencies dir it if it doesn't exist...
+mkdir -p ${DEPENDENCIESDIR}
 
 # start installation
+cd ${DEPENDENCIESDIR}
 echo "installing cppyy_backend"
-mkdir cppyy-backend
-chmod -R 777 cppyy-backend/
-sudo -u ${TOOLUSER} git clone https://github.com/wlav/cppyy-backend.git cppyy-backend/
-cd cppyy-backend
-sudo -u ${TOOLUSER} git checkout cppyy-cling-6.25.2
-cd cling
-sudo -u ${TOOLUSER} -E python3 setup.py egg_info
-sudo -u ${TOOLUSER} -E python3 create_src_directory.py
-sudo -u ${TOOLUSER} -E python3 -m pip install . --user --upgrade
-cd ../clingwrapper
-sudo -u ${TOOLUSER} -E python3 -m pip install . --user --upgrade --no-use-pep517 --no-deps
+if [ -d cppyy-backend ]; then
+	echo "${DEPENDENCIESDIR}/cppyy-backend already exists! Overwrite it?"
+	select result in Yes No; do
+		if [ "$result" == "Yes" ]; then
+			rm -rf cppyy-backend
+			break;
+		elif [ "$result" == "No" ]; then
+			break;
+		else
+			echo "please enter 1 or 2";
+		fi
+	done
+fi
+if [ ! -d cppyy-backend ]; then
+	mkdir cppyy-backend
+	chmod -R 777 cppyy-backend/
+	$SUDOFLAGS git clone https://github.com/wlav/cppyy-backend.git cppyy-backend/
+	cd cppyy-backend
+	$SUDOFLAGS git checkout -q cppyy-cling-6.25.2
+	cd cling
+	$SUDOFLAGS python3 setup.py egg_info
+	$SUDOFLAGS python3 create_src_directory.py
+	$SUDOFLAGS python3 -m pip install . $PIPFLAGS --upgrade
+	cd ../clingwrapper
+	$SUDOFLAGS python3 -m pip install . $PIPFLAGS --upgrade --no-use-pep517 --no-deps
+fi
 
 echo "installing CPyCppyy"
-cd ../../
-mkdir CPyCppyy
-chmod -R 777 CPyCppyy
-sudo -u ${TOOLUSER} git clone https://github.com/wlav/CPyCppyy.git CPyCppyy
-cd CPyCppyy
-git checkout CPyCppyy-1.12.8
-sudo -u ${TOOLUSER} -E python3 -m pip install . --user --upgrade --no-use-pep517 --no-deps
+cd ${DEPENDENCIESDIR}
+if [ -d CPyCppyy ]; then
+	echo "${DEPENDENCIESDIR}/CPyCppyy already exists! Overwrite it?"
+	select result in Yes No; do
+		if [ "$result" == "Yes" ]; then
+			rm -rf ${DEPENDENCIESDIR}/CPyCppyy
+			break;
+		elif [ "$result" == "No" ]; then
+			break;
+		else
+			echo "please enter 1 or 2";
+		fi
+	done
+fi
+if [ ! -d CPyCppyy ]; then
+	mkdir CPyCppyy
+	chmod -R 777 CPyCppyy
+	$SUDOFLAGS git clone https://github.com/wlav/CPyCppyy.git CPyCppyy
+	cd CPyCppyy
+	git checkout -q CPyCppyy-1.12.8
+	$SUDOFLAGS python3 -m pip install . $PIPFLAGS --upgrade --no-use-pep517 --no-deps
+fi
 
 echo "installing cppyy"
-cd ../
-mkdir cppyy
-chmod -R 777 cppyy
-sudo -u ${TOOLUSER} git clone https://github.com/wlav/cppyy.git cppyy
-cd cppyy
-sudo -u ${TOOLUSER} -E python3 -m pip install . --user --upgrade --no-deps --verbose
+cd ${DEPENDENCIESDIR}
+if [ -d cppyy ]; then
+	echo "${DEPENDENCIESDIR}/cppyy already exists! Overwrite it?"
+	select result in Yes No; do
+		if [ "$result" == "Yes" ]; then
+			rm -rf ${DEPENDENCIESDIR}/cppyy
+			break;
+		elif [ "$result" == "No" ]; then
+			break;
+		else
+			echo "please enter 1 or 2";
+		fi
+	done
+fi
+if [ ! -d cppyy ]; then
+	mkdir cppyy
+	chmod -R 777 cppyy
+	$SUDOFLAGS git clone https://github.com/wlav/cppyy.git cppyy
+	cd cppyy
+	$SUDOFLAGS git checkout -q 9bf3a2f6798066647e05d00d1c1736b453d336d3
+	$SUDOFLAGS python3 -m pip install . $PIPFLAGS --upgrade --no-deps --verbose
+fi
 
-echo "adding /home/${TOOLUSER}/.local/lib/python3.6/site-packages to PYTHONPATH"
-export PYTHONPATH=/home/${TOOLUSER}/.local/lib/python3.6/site-packages:$PYTHONPATH
+# get the local user site
+#if [ "${PIPFLAGS}" == "--user" ]; then
+#	PACKAGEPATH=$(python -m site --user-site)
+#else
+#	PACKAGEPATH= ????
+#fi
+PACKAGEPATH=$(python3 -m pip show cppyy_backend | grep 'Location' | cut -d' ' -f 2)
+echo "adding ${PACKAGEPATH} to PYTHONPATH"
+export PYTHONPATH=${PACKAGEPATH}:$PYTHONPATH
 
 echo "updating cppyy precompiled header"
-rm /home/${TOOLUSER}/.local/lib/python3.6/site-packages/cppyy/allDict.cxx.pch.*
+rm -f ${PACKAGEPATH}/cppyy/allDict.cxx.pch.*
 
 # a quick test, also trigger rebuilding of the pch
-echo "the following test should print 1 2 3 4 5 6 7 8 9"
+echo "the following test should print 0·1·2·3·4·5·6·7·8·9¶"
 RESULT=$(cat << EOF | python3
 import cppyy
 from cppyy.gbl.std import vector
@@ -117,6 +355,7 @@ for m in v: print(m, end=' ')
 
 EOF
 )
+
 # we need to do it twice as the first time it prints out a message about recompiling the header
 RESULT=$(cat << EOF | python3
 import cppyy
@@ -127,7 +366,9 @@ for m in v: print(m, end=' ')
 EOF
 )
 
-echo $RESULT
+# trim whitespace
+RESULT="$(echo -n ${RESULT} | xargs echo -n)"
+echo $RESULT | sed 's/ /·/g;s/\t/￫/g;s/\r/§/g;s/$/¶/g'  # check whitespace
 if [ "$RESULT" != "0 1 2 3 4 5 6 7 8 9" ]; then
 	echo "Test Failed! Check your installation of python3 and cppyy!"
 	exit 1
@@ -138,7 +379,7 @@ fi
 # add the path to cppyy module to the Setup.sh
 cat << "EOF" >> ${TOOLFRAMEWORKDIR}/Setup.sh
 
-export PYTHONPATH=/home/${TOOLUSER}/.local/lib/python3.6/site-packages:$PYTHONPATH
+export PYTHONPATH=/home/${THISUSER}/.local/lib/python3.6/site-packages:$PYTHONPATH
 
 EOF
 
