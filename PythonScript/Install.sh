ee917173aa1360eb49a37975f648f66e8e8c4bd1
marc1uk <marc1uk_@hotmail.com>
2022-11-15 11:15:47 +0000

    Update Install.sh

--- PythonScript/Install.sh
+++ PythonScript/Install.sh
@@ -161,7 +161,7 @@ fi
 
 # if the user wants to install pip dependencies into another account
 # we'll need sudo
-GOTSUDO=`which sudo > /dev/null; echo $?`
+GOTSUDO=`which sudo > /dev/null 2>&1; echo $?`
 SUDOCMD=""
 if [ ${GOTSUDO} -eq 1 ] && [ "$(whoami)" != "root" ]; then
 	SUDOCMD="sudo "
